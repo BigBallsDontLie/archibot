e9ba790590bcfd7358a912c04856ce9d41027bfb
BigBallsDontLie <johanhelmertz@hotmail.com>
2019-01-30 23:43:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var list = []
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Good morning') {
+    	message.reply('Good morning sunshine <3');
+  	}
+    var splitted = message.content.split(/ (.+)/);
+    if (splitted[0] === 'new') {
+    	list = []
+  	}
+    else if (splitted[0] === 'add') {
+    	list.push(splitted[1])
+        var listString = ''
+        for (i = 0; i < list.length; i++) {
+          listString += i + ". " + splitted[1] + "<br>";
+        }
+    	message.reply(listString);
   	}
 });
 
