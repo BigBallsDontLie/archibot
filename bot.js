5afd85fad308968c3e99ad62300156bb8df96c87
BigBallsDontLie <johanhelmertz@hotmail.com>
2019-02-10 22:22:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ client.on('message', message => {
                 if (multiSplit[3].toUpperCase() === 'TO') {
                     var rande = Math.random()
                     if (rande < 0.3) {
-                        message.reply('*teleported to ' + multiSplit[3] + '!!* Woop woop :D');
+                        message.reply('*teleported to ' + multiSplit[4] + '!!* Woop woop :D');
                     }
                     else if (rande < 0.6) {
                         message.reply('*teleport machine exploded* Whoops');
